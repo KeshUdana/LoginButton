1a0ec406cad60a1518a2b6db8723a19869f51e16
KeshUdana <125798571+KeshUdana@users.noreply.github.com>
2024-02-09 22:27:47 +0530

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,31 @@
+/*package com.example.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}*/
+package com.example.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class HelloController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    void showimage(MouseEvent event) {
+
+    }
+
+}
